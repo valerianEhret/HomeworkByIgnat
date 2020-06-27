84a1da731cad68294079d1943497f8832767b0e7
valerianEhret <vkadirovv@gmail.com>
2020-06-28 00:55:17 +0200

    add 5 homework added images in pages Junior, JuniorPlus

--- src/components/Junior/Junior.tsx
+++ src/components/Junior/Junior.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import classes from "./Junior.module.css"
 
 
 
@@ -6,7 +7,7 @@ import React from 'react';
 export function Junior() {
     return(
         <div>
-         Junior's Tasks
+            <img className={classes.img} src="https://media.proglib.io/wp-uploads/2018/08/baby_javascript.png" alt="baby with a laptop"/>
         </div>
     )
 
--- src/components/JuniorPlus/JuniorPlus.tsx
+++ src/components/JuniorPlus/JuniorPlus.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-
+import classes from "./JuniorPlus.module.css"
 
 
 
 export function JuniorPlus() {
     return(
         <div>
-        JuniorPlus's tasks
+            <img className={classes.img} src="https://images-na.ssl-images-amazon.com/images/I/81he0j5spJL._SX600_.jpg" alt=""/>
         </div>
     )
 
